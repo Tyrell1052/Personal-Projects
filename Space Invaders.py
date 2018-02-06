64b78ed0945dd821f11a7086b20942aa6db1bc79
Tyrell <Tyrell1052@gmail.com>
2018-02-06 11:56:25 -0500

    Space Invaders game created screen, player, and started movement

---
+++ Space Invaders.py
@@ -0,0 +1,51 @@
+import turtle
+import os
+
+#set up screen
+wn = turtle.Screen()
+wn.bgcolor('black')
+wn.title("Space Invaders")
+
+
+# Borders
+border_pen = turtle.Turtle()
+border_pen.speed(0)
+border_pen.color("white")
+border_pen.penup()
+border_pen.setposition(-300,-300)
+border_pen.pendown()
+border_pen.pensize(3)
+for side in range (4):
+    border_pen.fd(600)
+    border_pen.lt(90)
+border_pen.hideturtle()
+
+# create the player turtle
+player = turtle.Turtle()
+player.color("blue")
+player.shape("triangle")
+player.penup()
+player.speed(0)
+player.setposition(0, -250)
+player.setheading(90)
+
+
+# creating player movement
+playerspeed = 15
+
+
+# Move the player left and right
+def move_left():
+    x = player.xcor()
+    x -= playerspeed
+    player.set(x)
+
+# Creating keyboard bindings
+turtle.listen()
+turtle.onkey(move_left, "Left")
+
+
+
+
+
+delay = input("Press ender the quit game")
